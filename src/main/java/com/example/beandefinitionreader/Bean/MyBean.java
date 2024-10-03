a4e1e97206bda96ae2b6d54b9f8a87de613dd38f
Rhss <3567878896@qq.com>
2024-10-04 05:04:44 +0800

    XmlBeanDefinitionReader

---
+++ src/main/java/com/example/beandefinitionreader/Bean/MyBean.java
@@ -0,0 +1,21 @@
+package com.example.beandefinitionreader.Bean;
+
+public class MyBean {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/beandefinitionreader/BeanDefinitionReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.beandefinitionreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanDefinitionReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanDefinitionReaderApplication.class, args);
+    }
+
+}
